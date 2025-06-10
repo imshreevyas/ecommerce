4dad686dcc3666cde7b443121054a159efabe730
rohitvishin <rohitvishwakarma65@gmail.com>
2025-06-10 23:40:10 +0530

    20250610_adding_migration_table

--- database/migrations/2025_06_06_101631_create_orders_table.php
+++ database/migrations/2025_06_06_101631_create_orders_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('payment_status', ['pending', 'paid', 'failed', 'refunded'])->default('pending');
+            $table->decimal('mrp_amount', 10, 2);
+            $table->decimal('discount', 10, 2)->default(0);
+            $table->decimal('paid_amount', 10, 2);
+            $table->string('payment_id')->nullable();
+            $table->foreignId('coupon_id')->nullable()->constrained()->onDelete('set null');
+            $table->enum('order_status', ['pending', 'processing', 'shipped', 'delivered', 'cancelled'])->default('pending');
+            $table->string('customer_name');
+            $table->string('customer_phone');
+            $table->text('shipping_address');
+            $table->text('billing_address');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
